d0d68e18ea405c5af67816ae4fd2b0a24b23be59
Anubha Dubey <anubhadubeyy@gmail.com>
2022-04-05 12:38:22 -0400

    commit to master

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require ("express");
+var bodyParser = require ("body-parser");
+const app = express();
+var path = require("path");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+var newItems = ["Be Kind", "Smile", "Relax"];
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    // var today = new Date();
+    // console.log(today);
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var today  = new Date();
+    var finaldate= today.toLocaleDateString("en-US", options);
+    res.render("list", {dateToday:finaldate , listItems:newItems});
+});
+
+app.post("/", function(req, res){
+    newItem = req.body.task;
+    newItems.push(newItem);
+    res.redirect("/");
+})
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000...");
+})
