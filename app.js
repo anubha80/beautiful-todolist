d210797e6c257bc36f790afa6f80287bbd9c916d
Anubha Dubey <anubhadubeyy@gmail.com>
2022-04-05 13:39:42 -0400

    make changes to host on heroku

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.post("/", function(req, res){
     res.redirect("/");
 })
 
-app.listen(3000,function(){
+app.listen(process.env.PORT|| 3000,function(){
     console.log("Server is running on port 3000...");
 })
